f7c33f75596c6c01e986930e1669cb8d4c1c76ce
Trevor :D <TrevorRanney@gmail.com>
2019-02-21 18:36:56 -0700

    Added test coverage (requires node -v > 10) ; updated example

--- example/server.js
+++ example/server.js
@@ -4,10 +4,14 @@ function serveStuff(request,response){
     response.end("HELLO WORLD");
 }
 
+function serveDifferentStuff(request,response){
+    response.end("XD <3 XD");
+}
+
 var server = new Suit();
 server.httpPort = 8888;
 
-server.addHost("127.0.0.1:8888", serveStuff);
 server.addHost("localhost:8888", serveStuff); 
+server.addHost("127.0.0.1:8888", serveDifferentStuff);
 
 server.start();
