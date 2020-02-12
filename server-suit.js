311913710efec956d09c125ed98bc57c37f8b9e5
Trevor <Trevor.Ranney@gmail.com>
2020-02-12 08:30:57 -0700

    Simplifying things

--- server-suit.js
+++ server-suit.js
@@ -1,61 +1,62 @@
 
 var http = require('http');
-var https = require('https');
 var Router = require('./lib/router')
 
-var handlers = {};
-var middleware = [];
-var logger;
-var noHostHandler;
+// var 
+// var middleware = [];
+// var logger;
+// var noHostHandler;
 
 class Server{
     
     constructor(){
         this.httpPort = 80;
         this.httpsPort = 443;
+        this.handlers = {};
     }
 
-    setLogger(loggingModule){
-        logger = loggingModule;
-    }
+    // setLogger(loggingModule){
+    //     logger = loggingModule;
+    // }
 
-    setNoHostHandler(noHostHandlerModule){
-        noHostHandler = noHostHandlerModule;
-    }
+    // setNoHostHandler(noHostHandlerModule){
+    //     noHostHandler = noHostHandlerModule;
+    // }
 
     addHost(name, handler){
-        handlers[name] = handler;
+        this.handlers[name] = handler;
     }
 
     //test
-    addMiddleware(newMiddleware)
-    {
-        middleware.push(newMiddleware);
-    }
+    // addMiddleware(newMiddleware)
+    // {
+    //     middleware.push(newMiddleware);
+    // }
 
     handleRequest(request, response){
+        console.log("handle request",this)
         //test
-        middleware.forEach( (middle) =>
-        {
-            middle(request,response);
-        });
+        // middleware.forEach( (middle) =>
+        // {
+        //     middle(request,response);
+        // });
         var host = request.headers.host;
-        var handler = handlers[host];
-        if(logger)logger.logRequest(request);
+        var handler = this.handlers[host];
+        // if(logger)logger.logRequest(request);
         if(handler)
         {
             if(handler.respond)handler.respond(request, response);
             else handler(request,response);
         }
-        else {
-            if(noHostHandler)noHostHandler(request, response);
-        }
+        // else {
+        //     if(noHostHandler)noHostHandler(request, response);
+        // }
     }
 
     start(){
-        var server = http.createServer(this.handleRequest);
+        var server = http.createServer(this.handleRequest.bind(this));
         server.listen(this.httpPort, '0.0.0.0');
-
+        console.log("server",this);
         //could for each through a range of ports or accociate port with server
     //    var httpsServer = https.createServer(this.handleRequest);
     //    httpsServer.listen(this.httpsPort, '0.0.0.0');
