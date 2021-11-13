29655f540467d1985f7a9b8b24dfe2414a06270d
XD <XD>
2021-11-12 21:42:16 -0700

    optimized forlooper added simple array support

--- spec/server/forlooper.spec.js
+++ spec/server/forlooper.spec.js
@@ -2,12 +2,58 @@ const forloop = require('../../lib/forlooper');
 
 describe( "The forlooper", () => {
 
-    it("can loop", () => {
+    it("can do a simple loop", () => {
+        var videos = ["data","v2"];
+
+        expect(forloop('$for(video : videos){${video}}', videos))
+        .toBe("datav2");
+    });
+
+    it("can do a simple loop and replace multiple things", () => {
+        var videos = ["data","v2"];
+
+        expect(forloop('$for(video : videos){${video}${video}}', videos))
+        .toBe("datadatav2v2");
+    });
+
+    it("can do a simple loop with html", () => {
+        var videos = ["data","v2"];
+
+        expect(forloop('$for(video : videos){<div>${video}</div>}', videos))
+        .toBe("<div>data</div><div>v2</div>");
+    });
+
+    it("can add an index", () => {
+        var list = [7,7];
+        expect(forloop('$for(l : list){${index},}', list))
+        .toBe("0,1,");
+    });
+
+    it("can add multiple indexes", () => {
+        var list = [7,7];
+        expect(forloop('$for(l : list){${index}${index},}', list))
+        .toBe("00,11,");
+    });
+
+    it("can get array from object data", () => {
+        var variable = {videos: ["data","v2"]};
+
+        expect(forloop('$for(video : videos){${video}}', variable))
+        .toBe("datav2");
+    });
+
+    it("can loop through objects", () => {
+        var variable = {videos: [{mp4:"data"},{mp4: "v2"}]};
+
+        expect(forloop('$for(video : videos){${video.mp4}}', variable))
+        .toBe("datav2");
+    });
+
+    it("can loop through complex objects", () => {
         var variable = { user: {videos: [{mp4:"data"},{mp4: "v2"}]} };
 
         expect(forloop('$for(video : user.videos){${video.mp4}}', variable))
         .toBe("datav2");
     });
-
 
 });
--- spec/server/interpolater.spec.js
+++ spec/server/interpolater.spec.js
@@ -3,13 +3,14 @@ const interpolate = require('../../lib/interpolater');
 describe( "The interpolater", () => {
 
     it("can parse an arrays and complexe objects", () => {
-        var car = { body: { seats: ["drivers"]}};
+        var car = { body: { seats: ["drivers","pilots"]}};
         let template = "hello ${a[0][0].b.c}";
         let data = {a: [[{b: {c: "world",f: "hello"}}, 2], 3],d: 12,e: 14};
 
         expect(interpolate("${a[0][0].b.f} ${a[0][0].b.c}", data)).toBe("hello world");
-        expect(interpolate("${car.body.seats}", {car})).toBe("drivers");
-        expect(interpolate("${body.seats}", car )).toBe("drivers");
+        expect(interpolate("${car.body.seats}", {car})).toBe("drivers,pilots");
+        expect(interpolate("${car.body.seats[0]}", {car})).toBe("drivers");
+        expect(interpolate("${body.seats}", car )).toBe("drivers,pilots");
     });
 
     it("can parse simple Array", () => {
@@ -22,13 +23,54 @@ describe( "The interpolater", () => {
         expect(interpolate("${videos}",user )).toEqual('A,B');
     });
 
-    it("can parse simple object", () => {
+    it("can parse a simple object", () => {
         var vids = "vids";
         var user = {
             name: "Test",
             videos: vids
         };
         expect(interpolate("${user.videos}", {user} )).toEqual(vids);
+    });
+
+    it("can parse ", () => {
+        var vids = "vids";
+        var user = {
+            name: "Test",
+            videos: vids
+        };
+        expect(interpolate("${videos}", user )).toEqual(vids);
+    });
+
+    it("can very parse string", () => {
+        var user = "frank";
+        expect(interpolate("${user}", user )).toEqual(user);
+    });
+
+    it("can very parse string twice", () => {
+        var user = "frank";
+        expect(interpolate("${user} - ${user}", {user} )).toEqual("frank - frank");
+    });
+
+    it("can parse arrays", () => {
+        var variable = { letters: ['a','b']};
+        expect(interpolate('${letters}',variable))
+        .toBe('a,b');
+        expect(interpolate('${letters[1]}',variable))
+        .toBe('b');
+    });
+
+    it("can parse an array of objects", () => {
+        var objectList = [{mp4:"data", stuff:"things"},{mp4: "v2",stuff:"things"}];
+        var variable = { user: {videos: objectList} };
+        console.log(`${variable.user.videos}`)
+        console.log(`${variable.user.videos[1]}`)
+        console.log(`${variable.user.videos[1].mp4}`)
+        console.log("---")
+        console.log(interpolate('${user.videos}', variable))
+        console.log(interpolate('${user.videos}', variable)[1])
+        console.log(interpolate('${user.videos}', variable)[1].mp4)
+        expect(interpolate('${user.videos[1].mp4}', variable))
+        .toBe("v2");
     });
 
 });
